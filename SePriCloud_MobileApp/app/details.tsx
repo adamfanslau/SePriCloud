dda0e1be182beb0e6bc56c10dfb08784b9e6b2da
Adam Fanslau <adam.fanslau@ecreationmedia.tv>
2025-02-18 21:19:06 +0000

    added list of uploaded files and image picker to the app

---
+++ SePriCloud_MobileApp/app/details.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+interface IFileMetadata {
+        id: string;
+        datetime_added: Date;
+        filename: string;
+        tags: string | null,
+        description: string | null
+}
+
+export default function DetailsScreen() {
+    const [filesMetadata, setFilesMetadata] = useState<IFileMetadata[]>([]);
+
+    useEffect(() => {
+        fetch('http://192.168.1.7:3001/getAllFiles')
+            .then((res) => res.json())
+            .then((data) => {
+                setFilesMetadata(data);
+            });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Details</Text>
+            {filesMetadata.length > 0 ? (
+                filesMetadata.map((file, index) => (
+                    <>
+                    <Image key={file.id} source={{uri: `http://192.168.1.7:3001/files/${file.filename}`}} style={styles.image} />
+                    <Text key={file.filename}>{file.filename}</Text>
+                    <Text key={new Date(file.datetime_added).toISOString()}>{new Date(file.datetime_added).toISOString()}</Text>
+                    </>
+                ))
+            ) : (
+                <Text>No files found</Text>
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+    },
+});
--- SePriCloud_MobileApp/app/index.tsx
+++ SePriCloud_MobileApp/app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Button, Image, Text, View, StyleSheet } from "react-native";
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function Index() {
+export default function HomeScreen() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: false,
+      // aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container} >
+      <Text>SePriCloud</Text>
+      <Link href="/details">View details</Link>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
