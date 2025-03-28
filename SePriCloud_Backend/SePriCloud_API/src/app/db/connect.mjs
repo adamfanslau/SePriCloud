edcf5020428d3e51cf62a4fdea55a0ba1bf3f875
Adam Fanslau <adam.fanslau@gmail.com>
2025-03-28 21:26:43 +0000

    added env variables to the backend

--- SePriCloud_Backend/SePriCloud_API/src/app/db/connect.mjs
+++ SePriCloud_Backend/SePriCloud_API/src/app/db/connect.mjs
@@ -1,11 +1,13 @@
 import postgres from 'postgres';
 
+console.log('host: ', process.env.DB_HOST);
+
 const sql = postgres({
-  host: 'postgres',
-  port: 5432,
-  database: 'uploads',
-  username: 'postgres',
-  password: 'password',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 //   ssl: { rejectUnauthorized: false },
 });
 
